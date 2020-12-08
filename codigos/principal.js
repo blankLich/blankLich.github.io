1ad9e69601b16ffa489c4c0632abd461933d8221
blankLich <61538018+blankLich@users.noreply.github.com>
2020-12-07 23:28:00 -0300

    Add files via upload

--- codigos/principal.js
+++ codigos/principal.js
@@ -1,4 +1,18 @@
 let mylogo = document.querySelector('#logo');
+let opcaoJogo = document.querySelector('.jogos');
 mylogo.onclick = function() {
    alert("pare de tocar em mim!");
 }
+opcaoJogo.addEventListener('change', function(){
+   switch(this.value){
+      case 'Snake':
+         window.location.href = 'jogos/snake/index.html';
+         break;
+      case 'Breakout':
+         window.location.href = 'jogos/breakout/index.html';
+         break;
+      case 'Calculadora':
+         window.location.href = 'jogos/calculadora/index.html';
+         break;
+   }
+});
